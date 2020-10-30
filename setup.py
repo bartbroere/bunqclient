cb7d1aa1fb09d8c043fe2eaac193319a7c1971e9
Bart Broere <mail@bartbroere.eu>
2020-10-30 12:53:13 +0100

    Prepare for new release

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
 setup(
     name='bunqclient',
-    version='2017.7.18',
+    version='2020.10.30',
     description='Python client for the bunq public API',
     long_description=readme(),
     keywords=["bunq", "client", "bank", "api", "bunqclient"],
@@ -21,11 +23,11 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Office/Business :: Financial',
         'Topic :: Utilities',
     ],
