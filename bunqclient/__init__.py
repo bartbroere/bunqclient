83d67a819a61cd0ee8ba4db1ac087432b8d72daf
Bart Broere <2715782+bartbroere@users.noreply.github.com>
2020-10-30 12:29:38 +0100

    Fix PyCryptoDome new API spec

--- bunqclient/__init__.py
+++ bunqclient/__init__.py
@@ -1,5 +1,5 @@
 import base64
-import bunqclient.bunqdefault
+from .bunqdefault import headers, hierarchy
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
@@ -8,12 +8,12 @@ import pickle
 import requests
 import uuid
 
-class BunqClient(object):
+class BunqClient:
 
     def __init__(self, base="https://api.bunq.com/v1", secret=""):
         self.base, self.secret = base, secret.encode("latin1")
-        self.headers = bunqdefault.headers()
-        self.hierarchy = bunqdefault.hierarchy()
+        self.headers = headers()
+        self.hierarchy = hierarchy()
         if secret != "": self.create_session()
         return
 
@@ -23,22 +23,26 @@ class BunqClient(object):
         self.headers["X-Bunq-Client-Request-Id"] = str(uuid.uuid4())
         if "installation" not in k:
             self.headers["X-Bunq-Client-Signature"] = self.sign(
-                url[len(self.base)-3:], data=data, method=method).decode(
-                    'utf-8')
-        try: method = getattr(requests, method.lower())
-        except AttributeError: method = requests.get
+                url[len(self.base) - 3:], data=data, method=method).decode(
+                'utf-8')
+        try:
+            method = getattr(requests, method.lower())
+        except AttributeError:
+            method = requests.get
         return json.loads(method(url, data=data, headers=self.headers).text)
 
     def sign(self, url, **k):
         signeddata = [" ".join([k.get("method", "GET"), url])]
         for header, value in sorted(self.headers.items()):
             if (header in ['Cache-Control', 'User-Agent']) or (
-                header[0:6] == "X-Bunq"):
+                    header[0:6] == "X-Bunq"):
                 if header != "X-Bunq-Client-Signature":
                     signeddata.append(": ".join([header, value]))
         signeddata.extend(["", k.get("data", "")])
-        return base64.b64encode(self.signer.sign(SHA256.new().update(\
-            "\n".join(signeddata).encode("latin1"))))
+        sha256er = SHA256.new()
+        sha256er.update("\n".join(signeddata).encode("latin1"))
+        return base64.b64encode(self.signer.sign(sha256er))
+
 
     def prepare(self, **k):
         e = [self.base]
@@ -55,17 +59,17 @@ class BunqClient(object):
         if secret is not None: self.secret = secret
         self.create_rsasigner()
         self.installation = self.request(installation="", method="POST",
-            data={"client_public_key": self.rsakey.publickey().exportKey(
-                ).decode('utf-8').replace("RSA P", "P")+"\n"})
-        self.headers["X-Bunq-Client-Authentication"] = self.installation[\
+                                         data={"client_public_key": self.rsakey.publickey().exportKey(
+                                         ).decode('utf-8').replace("RSA P", "P") + "\n"})
+        self.headers["X-Bunq-Client-Authentication"] = self.installation[ \
             "Response"][1]["Token"]["token"]
         self.deviceserver = self.request(device_server="", method="POST",
-             data={"description": "bunqclient",
-                   "secret": self.secret.decode('utf-8')})
+                                         data={"description": "bunqclient",
+                                               "secret": self.secret.decode('utf-8')})
         self.deviceserver = self.deviceserver["Response"][0]["Id"]["id"]
         self.session = self.request(session_server="", method="POST",
-            data={"secret": self.secret.decode('utf-8')})
-        self.headers["X-Bunq-Client-Authentication"] = self.session[\
+                                    data={"secret": self.secret.decode('utf-8')})
+        self.headers["X-Bunq-Client-Authentication"] = self.session[ \
             "Response"][1]["Token"].get("token")
 
     def load_session(self, location):
